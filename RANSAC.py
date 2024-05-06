be91190937081f68a9d5ee3909a6ef0cb0df1970
Nikhil Ivaturi <nikhilivaturi25@gmail.com>
2024-05-05 20:46:52 -0400

    linear ransac completed

--- RANSAC.py
+++ RANSAC.py
@@ -1,36 +1,75 @@
 import numpy as np
 import numpy.linalg as npl
+import numpy.random as npr
 import scipy
 import random
+import matplotlib.pyplot as plt
 
-# def RANSAC_create(regression_type=None):
-#     if regression_type is None:
-#         regression_type = 'linear'
-
-#     if regression_type not in ['linear', 'quadratic']:
-#         raise Exception(f'regression type must be either linear or quadratic, not {regression_type}')
-
-#     match regression_type:
-#         case 'linear':
-#             return RANSAC_linear()
-
-class LinearRANSAC:
-    def __init__(self, k=50):
+class LinearRANSACModel:
+    def __init__(self, k=50, n=5):
         self.X = None
         self.y = None
 
-    def fit(self, X: np.ndarray, y: np.ndarray):
+        self.k = k
+        self.n = n
+        self.coefficients_ = None
+
+    def get_linear_mat(self, xs):
+        X = np.hstack((xs, np.ones((xs.shape[0], 1))))
+        return X
+
+    def fit(self, xs: np.ndarray, ys: np.ndarray):
+        self.n = min(self.n, xs.shape[0] - 1)
+
         best_coeffs = None
         best_score = np.inf
 
-        for i in range(self.k):
-            rand_X = np.array(random.sample(X, k=2))
-            coeffs = self.solve(*rand_X.flatten())
+        for _ in range(self.k):
+            rand_rows = npr.randint(xs.shape[0], size=(self.n))
+            coeffs = self.solve(xs[rand_rows], ys[rand_rows])
+
+            X = self.get_linear_mat(xs[rand_rows])
+            predicted = X @ coeffs
+
+            score = np.abs((predicted ** 2 - ys[rand_rows] ** 2).sum())
+
+            if score < best_score:
+                best_score = score
+                best_coeffs = coeffs
+
+        self.coefficients_ = best_coeffs
+
+    def transform(self, xs):
+        X = self.get_linear_mat(xs)
+        return X @ self.coefficients_
 
     def solve(self, xs, ys):
-        x1, x2 = xs
-        y1, y2 = ys
+        X = self.get_linear_mat(xs)
+        coeffs, residuals, rank, s = npl.lstsq(X, ys)
+        return coeffs
 
-        m = (y2 - y1) / (x2 - x1)
-        b = y1 - m * x1
+if __name__ == '__main__':
+    quadratic = False
+    if quadratic:
+        pass
+    else:
+        ransac = LinearRANSACModel(k=50, n=5)
+
+        x_dims = 1
+        num_x = 10
+
+        actual_coeffs = npr.random((x_dims + 1, 1)) * 5
+        xs = np.linspace(0, 10, num_x)[:, np.newaxis]
+        actual_ys = (ransac.get_linear_mat(xs) @ actual_coeffs) * (npr.random((num_x, 1)) * 0.1 + .9)
+
+        ransac.fit(xs, actual_ys)
+        print(f'coefficients: {ransac.coefficients_}')
+
+        predicted_ys = (ransac.get_linear_mat(xs) @ ransac.coefficients_)
+
+        plt.figure()
+        plt.scatter(xs, actual_ys)
+        plt.plot(xs, predicted_ys)
+
+        plt.show()
 
